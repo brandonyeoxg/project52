d862c6d12d0fcba2963379a7a9e275897e780471
brandon <brandonyeoxg@gmail.com>
2024-07-14 17:02:25 +0800

    feat: add configuration for API KEY

--- projects/project1/main.go
+++ projects/project1/main.go
@@ -1,7 +1,10 @@
 package main
 
+import "github.com/brandonyeoxg/project52/project1/config"
+
 func main() {
-	giphyProvider := newGiphy("<API KEY>")
+	config := config.New("config")
+	giphyProvider := newGiphy(config.GiphyAPIKEY)
 	s := newService(giphyProvider)
 	initRouter(s)
 }
